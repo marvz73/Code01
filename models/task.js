d62c82176b2827aa803fd35e645014b1506cbbce
root <root@ebpls.com>
2015-06-15 11:23:38 +0800

    completed field on task fix

--- models/task.js
+++ models/task.js
@@ -4,7 +4,7 @@ module.exports = function(sequelize, DataTypes) {
   var Task = sequelize.define("Task", {
     title: DataTypes.STRING,
     status: DataTypes.STRING,
-    completed: { type: Sequelize.BOOLEAN, allowNull: false, defaultValue: false},
+    completed: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false},
     desc: DataTypes.STRING,
     X: DataTypes.STRING,
     Y: DataTypes.STRING,
